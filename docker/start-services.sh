e1adf6c5ff5e6790b2622a3a5f77b2bb37b9f7ed
Nikola Stankovic <nikola.stankovic@viascom.ch>
2023-12-11 22:25:30 +0100

    fix: move arch folder creation to runtime (mounts override)

--- docker/start-services.sh
+++ docker/start-services.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir -p packages/{aarch64,armhf,armv7,ppc64le,s390x,x86,x86_64}
+
 lighttpd -D -f /etc/lighttpd/lighttpd.conf &
 "$HOME"/api --username local-dev --password local-dev --port 8081
