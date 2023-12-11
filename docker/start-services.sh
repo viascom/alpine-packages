e78a52a66766dfedabf6a7eae159027690e3a517
Nikola Stankovic <nikola.stankovic@viascom.ch>
2023-12-12 00:13:47 +0100

    feat: add placeholder envs for API run config

--- docker/start-services.sh
+++ docker/start-services.sh
@@ -3,4 +3,4 @@
 mkdir -p packages/{aarch64,armhf,armv7,ppc64le,s390x,x86,x86_64}
 
 lighttpd -D -f /etc/lighttpd/lighttpd.conf &
-"$HOME"/api --username local-dev --password local-dev --port 8081
+"$HOME"/api --username "$API_USERNAME" --password "$API_PASSWORD" --port "$API_PORT"
