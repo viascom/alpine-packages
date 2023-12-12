8a3738942103a23df2d1c1cdaacc76295034beed
Nikola Stankovic <nikola.stankovic@viascom.ch>
2023-12-12 14:08:06 +0100

    feat: add script to configure repositories

--- docker/configure-repositories.sh
+++ docker/configure-repositories.sh
@@ -3,5 +3,5 @@
 echo "https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 echo "https://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
 
-wget -P /etc/apk/keys https://cdn.azul.com/public_keys/alpine-signing@azul.com-5d5dc44c.rsa.pub
-echo "https://repos.azul.com/zulu/alpine" >> /etc/apk/repositories;
+wget -P /etc/apk/keys/ https://cdn.azul.com/public_keys/alpine-signing@azul.com-5d5dc44c.rsa.pub
+echo "https://repos.azul.com/zulu/alpine" | tee -a /etc/apk/repositories
